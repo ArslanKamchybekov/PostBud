7f75147e71f0757da616d104c60a322ee5332e3c
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-09-21 08:55:21 -0500

    Initial Commit

---
+++ PostBud/PostBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostBudApp.swift
+//  PostBud
+//
+//  Created by Arslan Kamchybekov on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PostBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PostBudTests/PostBudTests.swift
@@ -0,0 +1,17 @@
+//
+//  PostBudTests.swift
+//  PostBudTests
+//
+//  Created by Arslan Kamchybekov on 9/21/24.
+//
+
+import Testing
+@testable import PostBud
+
+struct PostBudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
